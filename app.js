aa0c5e32c0bda64128be5da24cf7ddef1c08455e
Tom Black <tom.black06007@gmail.com>
2021-03-20 23:11:12 -0700

    Added some additional functionality.

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
-const app = angular.module('flapperNews',[]);
+const app = angular.module('flapperNews', []);
 
 app.controller('MainCtrl', [
     '$scope',
-    function($scope){
+    function ($scope) {
         $scope.posts = [
-            {title: 'post1', upvotes: 5},
-            {title: 'post2', upvotes: 10},
-            {title: 'post3', upvotes: 15},
-            {title: 'post4', upvotes: 6},
-            {title: 'post5', upvotes: 9},
-            {title: 'post6', upvotes: 7}
+            { title: 'post1', upvotes: 5 },
+            { title: 'post2', upvotes: 10 },
+            { title: 'post3', upvotes: 15 },
+            { title: 'post4', upvotes: 6 },
+            { title: 'post5', upvotes: 9 },
+            { title: 'post6', upvotes: 7 }
         ];
-}]);
+        $scope.addPost = function () {
+            $scope.posts.push({ title: 'new post', upvotes: 0 });
+        }
+    }]);
