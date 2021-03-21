ca4fa35ed57934d996d094903706cb6fd76b05d3
Tom Black <tom.black06007@gmail.com>
2021-03-21 16:26:03 -0700

    Added the ability to increase the upvotes on a post.

--- app.js
+++ app.js
@@ -16,4 +16,7 @@ app.controller('MainCtrl', [
             $scope.posts.push({ title: $scope.title, upvotes: 0 });
             $scope.title = '';
         }
+        $scope.incrementUpvotes = function(post) {
+            post.upvotes+=1;
+        }
     }]);
