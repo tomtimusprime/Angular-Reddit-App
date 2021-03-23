1e699e860d847ec71afd2071439474f58f27b0d2
Tom Black <tom.black06007@gmail.com>
2021-03-22 22:05:40 -0700

    Added the a service to manage the posts and allow the posts to be injected into other services.

--- app.js
+++ app.js
@@ -1,16 +1,17 @@
 const app = angular.module('flapperNews', []);
 
+app.factory('posts', [function() {
+    var o = {
+        posts: []
+    }
+    return o;
+}])
+
 app.controller('MainCtrl', [
     '$scope',
-    function ($scope) {
-        $scope.posts = [
-            { title: 'post1', upvotes: 5 },
-            { title: 'post2', upvotes: 10 },
-            { title: 'post3', upvotes: 15 },
-            { title: 'post4', upvotes: 6 },
-            { title: 'post5', upvotes: 9 },
-            { title: 'post6', upvotes: 7 }
-        ];
+    'posts',
+    function ($scope, posts) {
+        $scope.posts = posts.posts;
         $scope.addPost = function () {
             if(!$scope.title || $scope.title === '') {return;}
             $scope.posts.push({ 
@@ -24,3 +25,12 @@ app.controller('MainCtrl', [
             post.upvotes+=1;
         }
     }]);
+
+    // [
+    //     { title: 'post1', upvotes: 5 },
+    //     { title: 'post2', upvotes: 10 },
+    //     { title: 'post3', upvotes: 15 },
+    //     { title: 'post4', upvotes: 6 },
+    //     { title: 'post5', upvotes: 9 },
+    //     { title: 'post6', upvotes: 7 }
+    // ];
